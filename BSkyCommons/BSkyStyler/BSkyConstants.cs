b1085ab047df931ca7552eafb93b285d27b49a4e
anil.dabral <anildabral3k@gmail.com>
2019-02-01 18:30:59 +0530

    fontsize changed

--- BSkyCommons/BSkyStyler/BSkyConstants.cs
+++ BSkyCommons/BSkyStyler/BSkyConstants.cs
@@ -3,6 +3,8 @@ namespace BSkyStyler
 {
     public static class BSkyConstants
     {
+        //Size for Courier New
+        /*
         //Constants for AUParaghraph control, in output window.
         public const int HEADER_FONTSIZE = 20;
         public const int HEADER_FONTSIZE2 = 18;
@@ -15,6 +17,22 @@ namespace BSkyStyler
 
         //Constants for AUParagraph (title etc.. not data) in Notes control
         public const int NOTES_HEADER_FONTSIZE = 14;//16;
+        */
 
+        //Size for Times New Roman
+
+        //Constants for AUParaghraph control, in output window.
+        public const int HEADER_FONTSIZE = 22;
+        public const int HEADER_FONTSIZE2 = 20;
+        public const int HEADER_FONTSIZE3 = 18;
+        public const int TEXT_FONTSIZE = 14;
+        public const int FOOTNOTE_FONTSIZE = 16;
+
+        //Constants for AUParagraph (grid title etc. not data) in AUXGrid (output grid) control.
+        public const int GRID_HEADER_FONTSIZE = 16;
+
+        //Constants for AUParagraph (title etc.. not data) in Notes control
+        public const int NOTES_HEADER_FONTSIZE = 16;
+        
     }
 }
