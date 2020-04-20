b1c4f00f4c710848a61f237af00e42322c4ea711
adtest <anildabral3k@gmail.com>
2020-04-20 22:34:22 +0530

    Feature to modify the font size in sytax editor has been added. This commit only contains the font-size window and the icon. Actual, code is in the outputwindow which is included in the next commit.

---
+++ BlueSkyProject/libs/BlueSky/Windows/SyntaxFontSettingsWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BlueSky.Windows
+{
+    /// <summary>
+    /// Interaction logic for SyntaxFontSettingsWindow.xaml
+    /// </summary>
+    public partial class SyntaxFontSettingsWindow : Window
+    {
+        public SyntaxFontSettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private double _fontsize;
+        public double FontSize
+        {
+            get { return _fontsize; }
+            set {
+                _fontsize = value;
+                fontslider.Value = value;
+            }
+        }
+        private void Fontsizecancelbtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Fontsizeokbtn_Click(object sender, RoutedEventArgs e)
+        {
+            _fontsize = fontslider.Value;
+            this.Close();
+        }
+    }
+}
